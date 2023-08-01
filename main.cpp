a368d06a83483aedd53f7498192de491c9d94308
Briand84 <brianmdignam@hotmail.com>
2023-08-01 19:40:25 +0000

    final commit
    Signed-off-by: Briand84 <brianmdignam@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@ microcontroller is not happy about)Green LED on mbed LED1 flashing SOS sequence*
 
 //reset button is printing "button pressed" instead of the blue user button which is (PC_13)
 
+/*used a switch function for the rise and fall of the user button (PC_13) on the mbed, now printing correctly, 
+added a sleep_for  statement between presses of 5 seconds*/
+
 
 
 
@@ -10,21 +13,34 @@ microcontroller is not happy about)Green LED on mbed LED1 flashing SOS sequence*
 
 // main() runs in its own thread in the OS
 InterruptIn bttn(PC_13);
-char flag0 = 1;
+char flag0 = 0;
 
 void fall_int(){
-     flag0 = 0;
+     flag0 = 1;
 }
 
-int main()
+void rise_int(){
+     flag0 = 0; 
 
-   {
+}
+
+int main(){
+    printf("Lab assignment 1\r\n");
+
+   
      bttn.fall(&fall_int);
+     bttn.rise(&rise_int);
      while(1){
-           printf("Button Pressed\r\n");
-           flag0 = 1;
-    break;
+         switch (flag0){
+                 case 1 :
+                 printf("Button Pressed\r\n");
+                 break;
+                 case 2:
+                 break;
          }
+         ThisThread::sleep_for(500);
+          }
+         
      }
        
 
