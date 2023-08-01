d710048804cfd4224814d3a1e50ad7c783e7c4d1
Briand84 <brianmdignam@hotmail.com>
2023-08-01 19:12:23 +0000

    commit 3 with comments
    Signed-off-by: Briand84 <brianmdignam@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 /*Error Message: Mutex: 0x200015FC, Not allowed in ISR context (Something happening in the SRAM location that the 
 microcontroller is not happy about)Green LED on mbed LED1 flashing SOS sequence*/
 
+//reset button is printing "button pressed" instead of the blue user button which is (PC_13)
+
 
 
 
@@ -8,19 +10,28 @@ microcontroller is not happy about)Green LED on mbed LED1 flashing SOS sequence*
 
 // main() runs in its own thread in the OS
 InterruptIn bttn(PC_13);
+char flag0 = 1;
 
-void bttn_InterruptIn(){
-     printf("Button Pressed\r\n");
+void fall_int(){
+     flag0 = 0;
 }
+
 int main()
-{
-    bttn.fall(bttn_InterruptIn);
-    while(true){
 
-    }
+   {
+     bttn.fall(&fall_int);
+     while(1){
+           printf("Button Pressed\r\n");
+           flag0 = 1;
+    break;
+         }
+     }
+       
+
+    
 
 
-}
+
 
 
 
